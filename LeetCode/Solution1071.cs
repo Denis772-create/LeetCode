6abd8903b5449b3f243633444317bd74d90029a3
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2023-12-28 22:00:51 +0500

    Solution1071

---
+++ LeetCode/Solution1071.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode;
+
+internal class Solution1071
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        //check if concatenation of two strings is the same
+        if (!(str1 + str2).Equals(str2 + str1)) return "";
+        //gcd of length of two string;
+        int gcdlength = gcd(str1.Length, str2.Length);
+        return str1.Substring(0, gcdlength);
+    }
+    public static int gcd(int x, int y)
+    {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+}

34f42c8ba1de14c89c941936cccc9ae051d9a063
Denis772-create <aleksandrovdenis418@gmail.com>
2024-01-01 19:37:59 +0500

    Solution1431

---
+++ LeetCode/Solution1431.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode;
+
+internal class Solution1431
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var maxCandiesAmongAll = candies.Max();
+        var candieswithExtrAmount = new bool[candies.Length];
+
+        for (var i = 0; i < candies.Length; i++)
+        {
+            candieswithExtrAmount[i] = candies[i] + extraCandies >= maxCandiesAmongAll;
+        }
+
+        return candieswithExtrAmount;
+    }
+}

7b26cb9097f2e0545c65c0f1e29e473dd836fc61
Denis772-create <aleksandrovdenis418@gmail.com>
2024-01-03 00:13:35 +0500

    Solution605

---
+++ LeetCode/Solution605.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode
+{
+    public class Solution605
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int count = 0;
+            int length = flowerbed.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    bool emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                    bool emptyRight = (i == length - 1) || (flowerbed[i + 1] == 0);
+
+                    if (emptyLeft && emptyRight)
+                    {
+                        flowerbed[i] = 1; // Plant a flower here
+                        count++;
+                    }
+                }
+            }
+
+            return count >= n;
+        }
+    }
+}

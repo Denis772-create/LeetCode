f33db4597176cf9e2940b959b810d2c93ebe218e
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2024-01-03 14:12:18 +0500

    M_Solution151

---
+++ LeetCode/M_Solution151.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode;
+
+internal class M_Solution151
+{
+    public string ReverseWords(string s)
+    {
+        var wordsArray = s.Split(' ');
+        var reversedWordsArray = wordsArray.Reverse();
+        return string.Join(" ", reversedWordsArray.Where(x => x != ""));
+    }
+}

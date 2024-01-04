43f289ffbaa75372d18ee9ffc34957441079492e
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2024-01-04 15:54:03 +0500

    M_Solution238

---
+++ LeetCode/M_Solution238.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode;
+
+internal class M_Solution238
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int n = nums.Length;
+        int[] answer = new int[n];
+
+        // First pass: calculate left products
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++)
+        {
+            answer[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        // Second pass: calculate right products and multiply with left products
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+}

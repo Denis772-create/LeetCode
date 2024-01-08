90d35eeab8ab995f882a39472b7af502a470b9d3
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2024-01-08 23:39:07 +0500

    M_Solution443

---
+++ LeetCode/M_Solution443.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode;
+
+internal class M_Solution443
+{
+    public int Compress(char[] chars)
+    {
+        int index = 0, i = 0;
+
+        while (i < chars.Length)
+        {
+            char currentChar = chars[i];
+            int count = 0;
+
+            while (i < chars.Length && chars[i] == currentChar)
+            {
+                i++;
+                count++;
+            }
+
+            chars[index++] = currentChar;
+            if (count > 1)
+            {
+                foreach (char c in count.ToString())
+                {
+                    chars[index++] = c;
+                }
+            }
+        }
+        return index;
+    }
+}

4f1510b55f26324f05d75f684f54914c961e7b66
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2024-01-09 13:35:33 +0500

    Solution283

---
+++ LeetCode/Solution283.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+internal class Solution283
+{
+    public void MoveZeroes(int[] nums)
+    {
+        // 'position' tracks the index where the next non-zero element should be placed.
+        var position = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                if (i != position)
+                {
+                    // Swap the non-zero element with the zero at the 'position' index.
+                    nums[position] = nums[i];
+                    nums[i] = 0;
+                }
+                position++;
+            }
+        }
+    }
+}

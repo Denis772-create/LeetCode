c7ce3be649680194b6392430495d70ef0841edcb
Denis772-create <aleksandrovdenis418@gmail.com>
2025-01-23 21:06:22 +0300

    1456

---
+++ LeetCode/Medium/1456.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Medium;
+
+public class _1456
+{
+    public int MaxVowels(string s, int k)
+    {
+        bool IsVowel(char c)
+        {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
+
+        int maxVowels = 0;
+        int currentVowels = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            if (IsVowel(s[i]))
+            {
+                currentVowels++;
+            }
+        }
+        maxVowels = currentVowels;
+
+        for (int i = k; i < s.Length; i++)
+        {
+            if (IsVowel(s[i - k]))
+            {
+                currentVowels--;
+            }
+
+            if (IsVowel(s[i]))
+            {
+                currentVowels++;
+            }
+            maxVowels = Math.Max(maxVowels, currentVowels);
+        }
+
+        return maxVowels;
+    }
+}

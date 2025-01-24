66887669a24e592029740e9a2e295822c92f060c
Denis772-create <aleksandrovdenis418@gmail.com>
2025-01-24 07:58:28 +0300

    123

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,11 +7,6 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            _136 solution = new _136();
-
-            Console.WriteLine(solution.SingleNumber([2, 2, 1])); // Output: 1
-            Console.WriteLine(solution.SingleNumber([4, 1, 2, 1, 2])); // Output: 4
-            Console.WriteLine(solution.SingleNumber([1])); // Output: 1        }
         }
     }
 }

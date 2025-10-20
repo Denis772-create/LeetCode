a0bdc375b1631d653e30e5e4d40e23a679198ced
Denis772-create <aleksandrovdenis418@gmail.com>
2025-10-20 21:17:07 +0300

    FindKthLargest

---
+++ LeetCode/Medium/Heap-PriorityQueue/FindKthLargest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium.Heap_PriorityQueue;
+
+public class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        var pq = new PriorityQueue<int, int>(k);
+
+        foreach (var num in nums)
+        {
+            if (pq.Count < k)
+            {
+                pq.Enqueue(num, num);
+            }
+            else if (num > pq.Peek())
+            {
+                pq.Dequeue();
+                pq.Enqueue(num, num);
+            }
+        }
+
+        return pq.Peek();
+    }
+}

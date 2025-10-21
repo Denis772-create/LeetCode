e34edfaa0ce8db659a0edf03919d8395506bc716
Denis772-create <aleksandrovdenis418@gmail.com>
2025-10-21 22:08:17 +0300

    Tribonacci

---
+++ LeetCode/Easy/DynamicProgramming/Tribonacci.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy.DynamicProgramming;
+
+public class Solution
+{
+    public int Tribonacci(int n)
+    {
+        switch (n)
+        {
+            case 0:
+                return 0;
+            case 1 or 2:
+                return 1;
+        }
+
+        int a = 0, b = 1, c = 1;
+        for (var i = 3; i <= n; i++)
+        {
+            var d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+}
---
+++ LeetCode/Medium/Backtracking/LetterCombinations.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Medium.Backtracking;
+
+public class Solution
+{
+    private static readonly Dictionary<char, string> map = new()
+    {
+        ['2'] = "abc",
+        ['3'] = "def",
+        ['4'] = "ghi",
+        ['5'] = "jkl",
+        ['6'] = "mno",
+        ['7'] = "pqrs",
+        ['8'] = "tuv",
+        ['9'] = "wxyz"
+    };
+    
+    public IList<string> LetterCombinations(string digits)
+    {
+        var res = new List<string>();
+        if (string.IsNullOrEmpty(digits)) return res;
+        
+        var path = new char[digits.Length];
+
+        void Dfs(int i)
+        {
+            if (i == digits.Length)
+            {
+                res.Add(new string(path));
+                return; 
+            }
+
+            if (!map.TryGetValue(digits[i], out var letters))
+                return; // на случай чужих символов
+
+            foreach (var ch in letters)
+            {
+                path[i] = ch;
+                Dfs(i + 1);
+            }
+        }
+        
+        Dfs(0);
+        return res;
+    }
+}

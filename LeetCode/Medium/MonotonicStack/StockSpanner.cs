48c492dd337b3c9b8921c67d98da91f6e8ce9bfd
Denis772-create <aleksandrovdenis418@gmail.com>
2025-10-23 09:08:14 +0300

    StockSpanner

---
+++ LeetCode/Medium/MonotonicStack/StockSpanner.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Medium.MonotonicStack;
+
+// [28],[14],[28],[35],[46],[53],[66],[80],[87],[88]
+public class StockSpanner
+{
+    // Храним пары (Price, Span) в монотонно невозрастающем по Price стеке.
+    private readonly Stack<(int Price, int Span)> _stack = new();
+    
+    public int Next(int price)
+    {
+        int span = 1;
+        
+        (int Price, int Days) current = (price, 1);
+        
+        // Сливаем все предыдущие блоки, цены которых ≤ текущей,
+        // суммируя их накопленный span.
+        while (_stack.Count > 0 && _stack.Peek().Price <= price)
+        {
+            span += _stack.Pop().Span;
+        }
+        
+        _stack.Push((price, span));
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */

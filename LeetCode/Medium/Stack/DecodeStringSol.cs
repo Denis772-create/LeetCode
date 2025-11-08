1bc90a181ecf1d7eff59c1fc73efdfa5eb345ac7
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-08 14:01:08 +0300

    ыфв

--- LeetCode/Medium/Stack/DecodeStringSol.cs
+++ LeetCode/Medium/Stack/DecodeStringSol.cs
@@ -1,75 +1,73 @@
 ﻿using System.Text;
-using System.Collections.Generic;
 
-namespace LeetCode.Medium.Stack
+namespace LeetCode.Medium.Stack;
+
+public class DecodeStringSol
 {
-    public class DecodeStringSol
+    public string DecodeString(string s)
     {
-        public string DecodeString(string s)
+        var st = new Stack<char>();
+
+        for (int i = s.Length - 1; i >= 0; i--)
         {
-            var st = new Stack<char>();
+            char ch = s[i];
 
-            for (int i = s.Length - 1; i >= 0; i--)
+            if (ch == '[')
             {
-                char ch = s[i];
-
-                if (ch == '[')
+                // 1) снять сегмент до ближайшей ']'
+                var seg = new StringBuilder();
+                while (st.Count > 0 && st.Peek() != ']')
                 {
-                    // 1) снять сегмент до ближайшей ']'
-                    var seg = new StringBuilder();
-                    while (st.Count > 0 && st.Peek() != ']')
-                    {
-                        seg.Append(st.Pop());
-                    }
-
-                    if (st.Count > 0)
-                    {
-                        st.Pop(); // убрать ']'
-                    }
-
-                    // 2) считать число слева от '[' (идём левее)
-                    int num = 0, place = 1;
-                    int j = i - 1;
-                    while (j >= 0 && char.IsDigit(s[j]))
-                    {
-                        num += (s[j] - '0') * place; // справа-налево
-                        place *= 10;
-                        j--;
-                    }
-
-                    // пропустить прочитанные цифры
-                    i = j + 1;
-
-                    // 3) положить сегмент num раз в СТЕК В ОБРАТНОМ порядке
-                    string chunk = seg.ToString();
-                    for (int r = 0; r < num; r++)
-                    {
-                        for (int k = chunk.Length - 1; k >= 0; k--)
-                        {
-                            st.Push(chunk[k]);
-                        }
-                    }
+                    seg.Append(st.Pop());
                 }
-                else if (char.IsDigit(ch))
+
+                if (st.Count > 0)
                 {
-                    // цифры пропускаем — они обрабатываются только вместе с '['
-                    continue;
+                    st.Pop(); // убрать ']'
                 }
-                else
+
+                // 2) считать число слева от '[' (идём левее)
+                int num = 0, place = 1;
+                int j = i - 1;
+                while (j >= 0 && char.IsDigit(s[j]))
                 {
-                    // буква или ']'
-                    st.Push(ch);
+                    num += (s[j] - '0') * place; // справа-налево
+                    place *= 10;
+                    j--;
+                }
+
+                // пропустить прочитанные цифры
+                i = j + 1;
+
+                // 3) положить сегмент num раз в СТЕК В ОБРАТНОМ порядке
+                string chunk = seg.ToString();
+                for (int r = 0; r < num; r++)
+                {
+                    for (int k = chunk.Length - 1; k >= 0; k--)
+                    {
+                        st.Push(chunk[k]);
+                    }
                 }
             }
-
-            // 4) собираем результат слева направо: просто снимаем со стека
-            var res = new StringBuilder(st.Count);
-            while (st.Count > 0)
+            else if (char.IsDigit(ch))
             {
-                res.Append(st.Pop());
+                // цифры пропускаем — они обрабатываются только вместе с '['
+                continue;
             }
-
-            return res.ToString();
+            else
+            {
+                // буква или ']'
+                st.Push(ch);
+            }
         }
+
+        // 4) собираем результат слева направо: просто снимаем со стека
+        var res = new StringBuilder(st.Count);
+        while (st.Count > 0)
+        {
+            res.Append(st.Pop());
+        }
+
+        return res.ToString();
     }
 }

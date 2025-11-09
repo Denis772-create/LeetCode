81a95cf1818511eb868886ca44e1a6dd5d9ad988
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-09 20:30:21 +0300

    TwoSum

---
+++ LeetCode/Easy/Arrays/TwoSum12.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Easy.Arrays;
+
+public class TwoSum1
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        // Словарь: число -> индекс
+        var map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i]; // число, которое нужно найти
+            
+            // Если в словаре уже есть нужное число — возвращаем индексы
+            if (map.ContainsKey(complement))
+            {
+                return [map[complement], i];
+            }
+            
+            // Иначе добавляем текущее число в словарь
+            map[nums[i]] = i;
+        }
+
+        // По условию задачи всегда есть одно решение
+        return [];
+    }
+}

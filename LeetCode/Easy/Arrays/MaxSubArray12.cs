17bbbef672f9ea298559fd162c6562fc78cd1dc8
d.aleksandrov <d.aleksandrov@cargonomica.com>
2025-11-11 11:39:27 +0300

    MaxSubArray

---
+++ LeetCode/Easy/Arrays/MaxSubArray12.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Easy.Arrays;
+
+public class MaxSubArray12
+{
+    // [-2,1,-3,4,-1,2,1,-5,4]
+    public int MaxSubArray(int[] nums)
+    {
+        int best = nums[0]; // лучшая сумма
+        int cur = nums[0];  // текущая сумма
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            // или продолжаем, или начинаем заново
+            cur = Math.Max(nums[i], cur + nums[i]);
+            best = Math.Max(best, cur);
+        }
+
+        return best;
+    }
+}

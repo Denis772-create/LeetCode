23cfe8776051fd4280f62ded5b417cad3e870419
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-12 20:43:28 +0300

    MaxProfit

---
+++ LeetCode/Easy/DynamicProgramming/MaxProfit12.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Easy.DynamicProgramming;
+
+public class MaxProfit12
+{
+    public int MaxProfit(int[] prices)
+    {
+        int minPrice = int.MinValue;
+        int maxProfit = 0;
+
+        foreach (var price in prices)
+        {
+            // если нашли цену меньше текущего минимума — обновляем
+            if (price < minPrice)
+            {
+                minPrice = price;
+            }
+            // иначе считаем возможную прибыль и обновляем максимум
+            else
+            {
+                int profit = price - minPrice;
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+        }
+        
+        return maxProfit;
+    }
+}

c96ef0414baec27419c248c264a15024d10e4d43
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-16 12:35:35 +0300

    asd

---
+++ LeetCode/Easy/Stack/ValidParentheses.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Easy.Stack;
+
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (c is '(' or '[' or '{')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0) 
+                    return false;
+                
+                var top = stack.Pop();
+                
+                switch (c)
+                {
+                    case ')' when top != '(':
+                    case ']' when top != '[':
+                    case '}' when top != '{':
+                        return false;
+                }
+            }
+        }
+        
+        return stack.Count == 0;
+    }
+}

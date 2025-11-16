4a1d779d19f8f7d31abe681ae341ee92146b94da
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-16 16:21:52 +0300

    GetIntersectionNode

---
+++ LeetCode/Easy/LinkedList/GetIntersectionNode132.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Easy.LinkedList;
+
+public class GetIntersectionNode132
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        var set = new HashSet<ListNode>();
+        
+        ListNode cur = headA;
+        while (cur != null)
+        {
+            set.Add(cur);
+            cur = cur.next;
+        }
+        
+        cur = headB;
+        while (cur != null)
+        {
+            if (set.Contains(cur))
+            {
+                return cur;
+            }
+        }
+        return null;
+    }
+}

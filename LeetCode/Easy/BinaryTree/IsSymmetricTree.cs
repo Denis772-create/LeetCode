b11f1264fe7ea0f4f7c775602afb69fc79504ad9
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-16 13:37:04 +0300

    IsSymmetric

---
+++ LeetCode/Easy/BinaryTree/IsSymmetricTree.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Easy.BinaryTree;
+
+public class IsSymmetricTree
+{
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+        public TreeNode? left = left;
+        public TreeNode? right = right;
+    }
+    
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null) return true;
+        return IsMirror(root.left, root.right);
+    }
+
+    private bool IsMirror(TreeNode? left, TreeNode? right)
+    {
+        // оба пустые — симметрично
+        if (left == null && right == null) return true;
+        
+        // один пустой, другой нет — не симметрично
+        if (left == null || right == null) return false;
+        
+        // значения должны совпадать
+        if (left.val != right.val) return false;
+        
+        return IsMirror(left.left, right.right) && IsMirror(left.right, right.left);
+    }
+}

ce35bae4b2497566a9fd54bb0918fb57446378c8
Denis772-create <aleksandrovdenis418@gmail.com>
2025-11-17 21:04:31 +0300

    LengthOfLongestSubstring

---
+++ LeetCode/Medium/SlidingWindow/LengthOfLongestSubstring.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Medium.SlidingWindow;
+
+public class LengthOfLongestSubstring3
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> window = new HashSet<char>(); // текущее окно без повторов
+        
+        
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            char c = s[right];
+            
+            // если символ уже в окне — сдвигаем left, пока он не исчезнет
+            while (window.Contains(c))
+            {
+                window.Remove(s[left]);
+                left++;
+            }
+            
+            // теперь символ уникален — добавляем его в окно
+            window.Add(c);
+            
+            // обновляем максимальную длину
+            maxLen = Math.Max(maxLen, right - left + 1);
+        }
+        
+        return maxLen;
+    }
+}
